e3c9e85c10a09da26d365717cc711cb07f1147e7
KuzMich1994 <drumer19940310@mail.ru>
2021-03-04 20:06:52 +0300

    Сверстал первые 3 секции, подключил слайдер Настроил кастомный селект с опциями. Осталось сделать адаптив

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,32 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
 import '../index.html';
 import '../css/style.css';
 import '../sass/style.sass';
+import dropdownSelect from './modules/dropdownSelect';
+
+const swiper = new Swiper('.catalog__slider', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 24,
+  width: 1831,
+
+  // If we need pagination
+  pagination: {
+    el: '.catalog__slide-pagination',
+    bulletClass: 'catalog__slide-pagination-bullet',
+    bulletActiveClass: 'catalog__slide-pagination-bullet_active',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.catalog__slide-arrow_next',
+    prevEl: '.catalog__slide-arrow_prev',
+  },
+});
+
+swiper.init();
+
+dropdownSelect();
---
+++ src/js/modules/dropdownSelect.js
@@ -0,0 +1,23 @@
+const dropdownSelect = () => {
+  const slide = document.querySelectorAll('.catalog__slide');
+
+  slide.forEach(item => {
+    item.addEventListener('click', e => {
+      const target = e.target;
+      const ul = item.querySelector('.catalog__slide-select-ul');
+      const output = item.querySelector('.catalog__slide-select-input');
+
+      if (target.matches('.catalog__slide-select')) {
+        ul.classList.toggle('catalog__slide-select-ul_active');
+        output.setAttribute('form', 'sell-form');
+      }
+      if (target.matches('.catalog__slide-select-cost')) {
+        output.value = target.textContent;
+        ul.classList.remove('catalog__slide-select-ul_active');
+        console.log(target.textContent);
+      }
+    });
+  });
+};
+
+export default dropdownSelect;
